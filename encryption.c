d65bec4b3344181b78e70680ae7b0f0bb86a42da
cmruderman <cruderman@Umass.edu>
2016-09-22 13:52:09 -0400

    First Commit

---
+++ encryption.c
@@ -0,0 +1,36 @@
+// Reads the input file 
+// character by character and then prints what it
+// just read.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+
+int main(int argc, char *argv[]) {
+
+	FILE *keyFile;
+    FILE *ifp;
+    FILE *ofp;
+    int inputChar;
+    int outputChar;
+    int prevChar;
+    int i;
+
+    if (argc < 3) printf("Input and output files need to be specified\n");
+    keyFile = fopen(argv[1], "r");
+    ifp = fopen(argv[2], "r");
+    ofp = fopen(argv[3], "w");
+
+    assert(ifp != NULL);
+    assert(ofp != NULL);
+
+
+     while ((inputChar = fgetc(ifp))!=EOF) {
+       fputc(inputChar, ofp); 
+     }
+    fclose(ifp);
+    fclose(ofp);
+
+}
