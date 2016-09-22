024b46a4c9228e2ea5275272b2984b141e0852c9
cmruderman <cruderman@Umass.edu>
2016-09-22 18:01:25 -0400

    Swap with points

--- encryption.c
+++ encryption.c
@@ -42,8 +42,8 @@ int main(int argc, char *argv[]) {
 
 }
 
-void swap(int a, int b){
-    int temp = a;
-    a = b;
-    b = temp;
+void swap(int* a, int* b){
+ int temp = *a;
+ *a = *b;
+ *b = temp;
 }
